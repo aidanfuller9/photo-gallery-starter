3ef6e41da39ed314281f8a039dc1e95d7e71a980
MichaelOppong <michael@scripted.org>
2020-06-18 03:31:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var money = 10.00;
-var cokes = 0;
-var chips = 0;
+    var money = 10.00;
+    var cokes = 0;
+    var chips = 0;
 
-$(".buy").click(function() {
-});
+    $(".buy").click(function() {
+        var item = $("input").val();
+        if (item === "coke" && money > 1.5) {
+            money = money - 1.5;
+            cokes = cokes + 1;
+            $(".message").html("I bought a coke for $1.50");
+        } else if (item === "chips" && money > 1.25) {
+            money = money - 1.25;
+            chips = chips + 1;
+            $(".message").html("I bought chips for $1.25");
+        } else {
+            $(".message").html("You can't buy that.");
+        }
+        $("#money").html("I have $" + money);
+    });
