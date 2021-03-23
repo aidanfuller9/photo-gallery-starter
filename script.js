a7400b83684feb9a8930944a67b178a4f24bf27a
Glitch (luxuriant-meowing-crowd) <none>
2021-03-23 00:57:25 +0000

    🛍💽 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let foodGallery = []
+
+ 
+
+$("button").click(function(){
+  // set a variable to grab the value of the input
+  let pictureUrl = $(".picture-url").val()
+  // add the variable to the foodGallery array
+  foodGallery.push(pictureUrl)
+  
+  console.log(foodGallery)
+  // loop through the foodGallery and append to HTML
+  for(let food in foodGallery){
+  $(".gallery").append(food)
+} 
+})
+
 
